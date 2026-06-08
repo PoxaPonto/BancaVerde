69ce3faad930a559c490f1dc068c619d3a25abeb
PoxaPonto <poxaponto@gmail.com>
2026-06-08 02:31:08 -0300

    Executa migrations automaticamente no deploy

--- beckend/BancaVerdeAPI/Program.cs
+++ beckend/BancaVerdeAPI/Program.cs
@@ -117,6 +117,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseSerilogRequestLogging();
 
 app.UseMiddleware<ExceptionMiddleware>();
